e0f4d47672e7cb8e106727fdfef95bf4ab1dd0a0
BristolEcru <englishteachingin2020@gmail.com>
2023-02-06 22:45:16 +0100

    Task Day 6 Listof Employees with scores and min/max/avg value

---
+++ ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public List<int> points= new List<int>();
+
+
+        Random rng = new Random();
+
+        public Employee(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public void AddPointsToEmployee(int score)
+        {
+            points.Add(score);
+        }
+        public void AddRandomPointsToEmployee()
+        {
+            AddPointsToEmployee(rng.Next(1, 11));
+        }
+
+        public void ShowThePointsOfEmp()
+        {
+            foreach (var i in points)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            Console.Write("Suma: ");
+            Console.WriteLine(SumOfPointsOfEmployee());
+            Console.WriteLine();
+        }
+
+        public int SumOfPointsOfEmployee()
+        {
+            return points.Sum();
+        }
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+
+
+Employee emp1 = new Employee(" Michał ", " Adamski ", 40);
+Employee emp2 = new Employee(" Ewa ", " Myśliwiec ", 30);
+Employee emp3 = new Employee(" Radek ", " Duski ", 33);
+
+List<Employee> employeesList = new List<Employee>();
+
+employeesList.Add(emp1);
+employeesList.Add(emp2);  
+employeesList.Add(emp3);
+
+int maxResult = 0;
+int minResult = 1000;
+Employee EmployeeWithMAXScore = new Employee(null, null, 0);
+Employee EmployeeWithMINScore = new Employee(null, null, 0);
+float totalOfPoints = 0;
+
+foreach (Employee emp in employeesList)
+{
+    emp.AddRandomPointsToEmployee();
+    emp.AddRandomPointsToEmployee();
+    emp.AddRandomPointsToEmployee();
+
+}
+
+foreach (Employee emp in employeesList)
+{
+    Console.WriteLine($"{emp.Name.ToString()}:");
+    emp.ShowThePointsOfEmp();
+
+}
+foreach (Employee emp in employeesList)
+{
+    var sumofpoints = emp.SumOfPointsOfEmployee();
+    totalOfPoints += sumofpoints;
+
+    if (sumofpoints > maxResult)
+    {
+        EmployeeWithMAXScore = emp;
+        maxResult= sumofpoints;
+    }
+    if (sumofpoints < minResult)
+
+    {
+        EmployeeWithMINScore = emp;
+        minResult= sumofpoints;
+    }
+
+}
+
+
+
+Console.Write("The winner of max is: "); Console.WriteLine(EmployeeWithMAXScore.Name.ToString());
+Console.Write("The winner of min is: "); Console.WriteLine(EmployeeWithMINScore.Name.ToString());
+Console.WriteLine($"The average score is : {totalOfPoints/employeesList.Count:N2}");
+
+
+
+
+Console.WriteLine();
